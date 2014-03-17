2142ef1c63469c130625e1d62e8f7941f2272923
Francois Lacoursiere <flacours@gmail.com>
2014-03-17 12:59:55 -0400

    computecost not completed

--- mlclass-ex1/computeCost.m
+++ mlclass-ex1/computeCost.m
@@ -9,6 +9,19 @@ m = length(y); % number of training examples
 % You need to return the following variables correctly 
 J = 0;
 
+errorsum = 0;
+for( i = 1 : m)
+  disp(sprintf('theta 1 : %f', theta(1)));
+  disp(sprintf('theta 2 : %f', theta(2)));
+disp(sprintf('X(1,i) : %f', X(1,i)));
+  h0 = theta(1) + theta(2) * X(1,i);
+disp(h0);
+errorsum += (h0-y(i))^2;
+disp(sprintf('errorsum %f', errorsum));
+end
+
+J=1/(2*m)*errorsum;
+
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
